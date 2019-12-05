dd5f2c5638ef9154770253bfa4da082c42f91a86
kason <718087538@qq.com>
2019-12-05 10:15:59 +0800

    该轮播图

--- pages/chapter.vue
+++ pages/chapter.vue
@@ -7,7 +7,7 @@
         <nuxt-link to="#">应用</nuxt-link>
       </li>
     </ul>
-    <el-pagination background layout="prev, pager, next" :total="10"></el-pagination>
+    <!-- <el-pagination background layout="prev, pager, next" :total="10"></el-pagination> -->
   </div>
 </template>
 
@@ -24,7 +24,7 @@ export default {
   async asyncData({ params, query }) {
     console.log("打印出查询的", query);
     let { data } = await axios.get(
-      `http://127.0.0.1:7001/api/chaper?cid=${query.cate}`
+      `http://127.0.0.1:7001/api/client/chapter?cid=${query.cate}`
     );
     console.log("sss", data.data);
     return {
